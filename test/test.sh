cf7595ecf597a6944e9f47aab41357b62b8601d7
Jose Fernandez de Castro <jose.fernandez@flatironschool.com>
2023-01-31 12:25:32 -0500

    Added sample test

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-lg -r p -m "This one passed" -e "Test explanation"
-lg -r p -m "This one also passed"
+# Store script output
+output=()
+while read -r line
+do
+  output+=("$(tr [A-Z] [a-z] <<< "$line")")
+done < <(sh script.sh)
+
+# Test every line
+if [[ ${output[0]} -eq "hello" ]]
+then
+  lg -r p -m "First line is hello!"
+else
+  lg -r f -m "First line is not hello!" -e "Make sure your first line outputs hello." 
+fi
+
+if [[ ${output[1]} -eq "goodbye" ]]
+then
+  lg -r p -m "Second line is goodbye!"
+else
+  lg -r f -m "Second line is not goodbye!" -e "Make sure your second line outputs goodbye." 
+fi
